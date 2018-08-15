9b4b8b537414cdd44c12447118fbfd96d13021fc
sutaojie <sutaojie@gmail.com>
2018-08-15 10:58:15 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  <to-aside/>
+  </div>
+</template>
+
+<script>
+import ToAside from './components/ToAside'
+export default {
+  name: 'App',
+  components:{
+    ToAside
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ToAside.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="aside">
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="sass" scoped>
+    .aside
+        width: 200px
+        height: 100vh
+        border: 1px solid red
+    
+</style>
